5b1eb8cac13856b9c010b2616aa20c1c19042f36
Nicolas Gomez R <cafelusitania@gmail.com>
2020-12-07 23:09:02 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// Person Age
+var Age = prompt('please type your age');
+alert('Hi my name is Nick and this is ConvertAlert.');
+alert(Date());
+confirm('¿ Is that your age ?');
+console.log(Age)
+// Age Database
+const db = new Database(Age);
+const Database = require(Age);
+
+// Person name
+var Name = prompt('Please type your Name');
+alert('Hola ' + Name);
+confirm('¿ is that your name ?')
+// Name Database
+const db = new Database(Name);
+const Database = require(Name);
